b64fa8e6c85c5bc44df7e3cbc35f54b2dc236891
mimrancomsats <66168186+mimrancomsats@users.noreply.github.com>
2023-05-28 17:10:38 +0500

    Update FirstTestScript.py

--- FirstTestScript.py
+++ FirstTestScript.py
@@ -3,17 +3,12 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 options = Options()
-
 options.headless = True
-
 driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
-
-#driver = webdriver.Chrome()
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
 title = driver.title
-#print(title)
 assert title == "Web form"
 
 driver.implicitly_wait(0.5)
@@ -21,12 +16,11 @@ driver.implicitly_wait(0.5)
 text_box = driver.find_element(by=By.NAME, value="my-text")
 submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-
 text_box.send_keys("Selenium")
 submit_button.click()
 
 message = driver.find_element(by=By.ID, value="message")
 value = message.text
-#print(value)
+
 assert value == "Received!"
 driver.quit()
