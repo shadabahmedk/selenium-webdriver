a44eaf3265350164874587654ce5c33476a2c3d4
mimrancomsats <66168186+mimrancomsats@users.noreply.github.com>
2023-11-25 13:45:44 +0500

    Create updatedDataEntryTest.py

---
+++ updatedDataEntryTest.py
@@ -0,0 +1,31 @@
+# vim test.py
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+title = driver.title
+assert title == "Web form"
+
+driver.implicitly_wait(0.5)
+
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+text_box.send_keys("Selenium")
+submit_button.click()
+
+message = driver.find_element(by=By.ID, value="message")
+value = message.text
+
+assert value == "Received!"
+driver.quit()
